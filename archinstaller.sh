8fac9ab41f2275e16d3abc4e67a2c7a1fd73b5bf
ali3b <andrew.liebenberg@gmail.com>
2021-12-07 22:18:23 +1100

    commit

--- archinstaller.sh
+++ archinstaller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/ArchInstaller/1-setp.sh
+    arch-chroot /mnt /root/ArchInstaller/1-setup.sh
     source /mnt/root/ArchInstaller/install.conf
     arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchInstaller/2-user.sh
     arch-chroot /mnt /root/ArchInstaller/3-post-setup.sh
