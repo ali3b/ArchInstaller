a4b9f33fdeaca510415a2affaf540fc46462537e
ali3b <andrew.liebenberg@gmail.com>
2021-12-07 22:21:26 +1100

    commit

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -23,6 +23,50 @@ if [[ -d "/sys/firmware/efi" ]]; then
     grub-install --efi-directory=/boot ${DISK}
 fi
 grub-mkconfig -o /boot/grub/grub.cfg
+# ------------------------------------------------------------------------
+
+echo
+echo "Genaerating .xinitrc file"
+
+# Generate the .xinitrc file so we can launch XFCE from the
+# terminal using the "startx" command
+cat <<EOF > ${HOME}/.xinitrc
+#!/bin/bash
+
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+    for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+        [ -x "\$f" ] && . "\$f"
+    done
+    unset f
+fi
+
+source /etc/xdg/xfce4/xinitrc
+exit 0
+EOF
+
+# ------------------------------------------------------------------------
+
+echo
+echo "Updating /bin/startx to use the correct path"
+
+# By default, startx incorrectly looks for the .serverauth file in our HOME folder.
+sudo sed -i 's|xserverauthfile=\$HOME/.serverauth.\$\$|xserverauthfile=\$XAUTHORITY|g' /bin/startx
+# ------------------------------------------------------------------------
+
+echo
+echo "Increasing file watcher count"
+
+# This prevents a "too many files" error in Visual Studio Code
+echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
+
+# ------------------------------------------------------------------------
+
+echo
+echo "Disabling Pulse .esd_auth module"
+
+# Pulse audio loads the `esound-protocol` module, which best I can tell is rarely needed.
+# That module creates a file called `.esd_auth` in the home directory which I'd prefer to not be there. So...
+sudo sed -i 's|load-module module-esound-protocol-unix|#load-module module-esound-protocol-unix|g' /etc/pulse/default.pa
 
 # ------------------------------------------------------------------------
 
